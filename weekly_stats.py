43d28a9698105a8a84eb5307795396e27880ee03
osherboudara99 <osherboudara99@gmail.com>
2026-02-05 17:39:53 -0500

    began writing stats data ingestion scripts

--- weekly_stats.py
+++
@@ -1,7 +0,0 @@
-import nflreadpy as nfl 
-import pandas as pd
-
-
-seasonal = nfl.load_player_stats(seasons=True, summary_level='reg').to_pandas()
-weekly = nfl.load_player_stats(summary_level="week").to_pandas()
-
