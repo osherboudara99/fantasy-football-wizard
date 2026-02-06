be4b7bc8a0ca0820545f37f262fd4daed67aa945
osherboudara99 <osherboudara99@gmail.com>
2026-02-05 20:16:28 -0500

    wrote stats class

---
+++ scripts/weekly_stats_endpoint.py
@@ -0,0 +1,66 @@
+import nflreadpy as nfl 
+from pathlib import Path
+from typing import Literal
+import polars as pl
+import pandas as pd
+
+
+class Stats:
+    def __init__(self, seasons: Literal['all', 'current'] | list[int], 
+                 aggregation: Literal['week', 'reg', 'post', 'reg+post']): 
+        if isinstance(seasons, str):
+            if seasons == 'all':
+                seasons_source_arg = True
+            elif seasons == 'current':
+                seasons_source_arg = None 
+            else:
+                raise ValueError(f"Invalid seasons string: {seasons}. Must be 'all', 'current' or list[int].")
+        elif isinstance(seasons, list):
+            seasons_source_arg = seasons
+        else:
+            raise TypeError(f"seasons must be str or list[int]")
+
+        self.data = nfl.load_player_stats(seasons=seasons_source_arg, summary_level=aggregation)
+    
+    def null_removal(self, columns:list[str]) -> pd.DataFrame | pl.DataFrame:
+        self.data = self.data.dropna(subset=columns)
+        return self.data
+    
+    def null_summary(self):
+        if isinstance(self.data, pd.DataFrame):
+            return self.data.isnull().sum()
+        elif isinstance(self.data, pl.DataFrame):
+            return self.data.null_count()
+        else:
+            raise TypeError("Unsupported DataFrame type.")
+        
+    def save_parquet(self, file_name: str, stage: Literal["raw", "processed"] = "raw", base_dir: Path | str = "data"):
+        base_dir = Path(base_dir)
+
+        stage_dirs = {
+            "raw" : base_dir / "raw" / "stats",
+            "processed" : base_dir / "processed"
+        }
+
+        output_dir = stage_dirs[stage]
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        output_path = output_dir / file_name
+
+        if isinstance(self.data, pl.DataFrame):
+            self.data.write_parquet(output_path)
+        elif isinstance(self.data, pd.DataFrame):
+            self.data.to_parquet(output_path)
+        else:
+            raise TypeError("self.data must be a Pandas or Polars DataFrame.")
+
+        return output_path  
+
+
+
+    
+
+
+
+weekly = nfl.load_player_stats(summary_level="week").to_pandas()
+
